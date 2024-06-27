ebdc7f0048c436fccd813794cfe3a181ad55cf88
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-06-27 19:31:46 -0300

    Add files via upload

---
+++ aula12.py
@@ -0,0 +1,9 @@
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = peso / (altura ** 2)
+print(imc)
+
+# Luiz otávio tem 1.80 de altura,
+# pesa 95 quilos e seu imc é
+# 29.32098
--- aula7.py
+++ aula7.py
@@ -11,4 +11,5 @@
 nome = 'Luiz'
 idade = 30
 maior_de_idade = idade>=18
-print('Nome: ', nome)
+print('Nome: ', nome, 'Idade: ', idade)
+print('É maior?', maior_de_idade)
