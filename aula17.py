05681b59edf935098301f4dbdd66946434e7f151
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-06-29 23:03:47 -0300

    Add files via upload

---
+++ aula17.py
@@ -0,0 +1,17 @@
+#if / elif / else
+
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+    print('Código para a condição 1')
+elif condicao2:
+    print('Código para a condição 2')
+elif condicao3:
+    print('Código para a condição 3')
+elif condicao4:
+    print('Código para a condição 4')
+else:
+    print('Nenhuma condição foi satisfeita')
