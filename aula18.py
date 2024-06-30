66a57c02442867aa1462d19d9b8b30956c0a9ac1
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-06-30 20:20:31 -0300

    Add files via upload

---
+++ aula18.py
@@ -0,0 +1,23 @@
+# if / elif / else
+#if / elif / else
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Código para a condição 1')
+elif condicao2:
+    print('Código para a condição 2')
+elif condicao3:
+    print('Código para a condição 3')
+elif condicao4:
+    print('Código para a condição 4')
+else:
+    print('Nenhuma condição foi satisfeita')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
