b08504f6bc8335f5cf3f1a1214cccf3eec376d5c
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-07-06 18:36:38 -0300

    Add files via upload

--- aula42.py
+++ aula42.py
@@ -1,3 +1,18 @@
 frase = 'O Python é uma linguagem de programação multiparadigma' 'Python foi criado por Guido van Rossum.'
 
-print(frase.count)
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+while i < len(frase):
+    letra_atual = frase[i]
+    qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
+
+    if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
+        qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
+        letra_apareceu_mais_vezes = letra_atual
+
+
+    print(letra_atual, qtd_apareceu_mais_vezes_atual)
+    i += 1
+
+print(f'A letra que apareceu')
