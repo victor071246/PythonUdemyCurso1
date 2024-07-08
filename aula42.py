6e2e2a97e52d8198d527067d8fc0df1cef1670aa
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-07-07 22:13:51 -0300

    Add files via upload

--- aula42.py
+++ aula42.py
@@ -5,6 +5,10 @@ qtd_apareceu_mais_vezes = 0
 letra_apareceu_mais_vezes = ''
 while i < len(frase):
     letra_atual = frase[i]
+
+    if letra_atual == '':
+        continue
+
     qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
 
     if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
@@ -12,7 +16,7 @@ while i < len(frase):
         letra_apareceu_mais_vezes = letra_atual
 
 
-    print(letra_atual, qtd_apareceu_mais_vezes_atual)
     i += 1
 
-print(f'A letra que apareceu')
+print(f'A letra que apareceu mais vezes foi{letra_apareceu_mais_vezes}, aparecendo {qtd_apareceu_mais_vezes} vezes')
+      
