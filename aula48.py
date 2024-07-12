11883bdcbd13208793b6e8de11d212039d7452bb
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-07-11 23:11:50 -0300

    Add files via upload

---
+++ aula48.py
@@ -0,0 +1,67 @@
+"""
+Listas em python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: 
+ append -> adiciona um item ao final da lista
+ insert -> adiciona um item no índice escolhido
+ pop    -> remove do final ou um índice escolhido
+ del    -> apaga o índice
+ clear  -> limpa a lista
+ extend -> estende a lista
+ + -    -> concatena as listas 
+Create Read Update Delete
+Criar, Ler, Atualizar, Deletar = lista[i] (CRUD)
+"""
+
+
+"""
+Cuidados com dados mutáveis
+= -> copiando o valor (imutáveis)
+= -> aponta para o mesmo valor na memória (mutável)
+"""
+nome = 'Luiz'
+nome[1] = 'Luiz'
+
+# lista_a = [1, 2, 3]
+# lista_b = [4, 5, 6]
+# lista_c = lista_a + lista_b
+# lista_a.extend(lista_b)
+# print(lista_a) 
+
+
+
+
+
+
+# lista = [10, 20, 30, 40]
+# lista.append('Luiz')
+# nome = lista.pop()
+# lista.append(1233)
+# del lista[-1]
+# #lista.clear()
+# lista.insert(0, 5)
+# print(lista)
+
+
+
+
+
+
+
+
+
+# # lista[2] = 300
+# # del lista[2]
+# # print(lista)
+# # print(lista[2])
+# lista.append(50)
+# lista.pop()
+# lista.append(60)
+# lista.append('BBB')
+# ultimo_valor = lista.pop(3)
+# print(lista, 'Removido', ultimo_valor)
+
+
+
