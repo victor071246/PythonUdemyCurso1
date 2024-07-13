4d2fa6b6a1b0806a63dc10767add71fea7f9ee65
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-07-13 17:39:03 -0300

    Add files via upload

---
+++ aula54.py
@@ -0,0 +1,5 @@
+"""
+Faça uma lista de comprar com listas
+O usuário deve ter a possibilidade de inserir, apagar e listar valores de sua lista
+Não permita que o programa quebre com erros de índices inexistentes na lista.
+"""
