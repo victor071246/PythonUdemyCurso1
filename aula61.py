e6570529e4fda084cef4a24a446c66b3ef3c0ab7
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-07-18 23:06:28 -0300

    Add files via upload

--- aula61.py
+++ aula61.py
@@ -22,5 +22,19 @@ O primeiro dígito do CPF é 7
 """
 cpf = '74682489070'
 nove_digitos = cpf[:9]
+contador_regressivo_1 = 10
 
-print(nove_digitos)
+resultado_digito_1 = 0
+for digito_1 in nove_digitos:
+    resultado_digito_1 += int(digito_1) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+digito_1 = (resultado_digito_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
+
+resultado_digito_2 = 0
+for digito in dez_digitos:
+    print(int(digito) * contador_regressivo_2)
+    contador_regressivo_2 -= 1
