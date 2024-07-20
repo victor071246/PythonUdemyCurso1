1f71db8d3152298d4e5719e9047c12843331a74c
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-07-19 22:25:10 -0300

    Add files via upload

--- aula61.py
+++ aula61.py
@@ -20,8 +20,8 @@ contrário disso:
 
 O primeiro dígito do CPF é 7
 """
-cpf = '74682489070'
-nove_digitos = cpf[:9]
+cpf_enviado_usuario = '74682489070'
+nove_digitos = cpf_enviado_usuario[:9]
 contador_regressivo_1 = 10
 
 resultado_digito_1 = 0
@@ -36,5 +36,15 @@ contador_regressivo_2 = 11
 
 resultado_digito_2 = 0
 for digito in dez_digitos:
-    print(int(digito) * contador_regressivo_2)
+    resultado_digito_2 += int(digito) * contador_regressivo_2
     contador_regressivo_2 -= 1
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+print(cpf_gerado_pelo_calculo)
+
+if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
+    print(f'{cpf_enviado_usuario} é valido')
+else:
+    print('CPF inválido')
