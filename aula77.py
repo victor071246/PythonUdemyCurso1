856ec2a75fe4ee42483aec823fb502db6696ac67
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-07-26 23:32:07 -0300

    Add files via upload

--- aula77.py
+++ aula77.py
@@ -17,5 +17,40 @@ perguntas = [
         'Resposta': '5',
     },   
 ]
+qtd_acertos = 0
+qtd_perguntas = len(perguntas)
 for pergunta in perguntas:
-    print(pergunta[])
+    print('Pergunta', pergunta['Pergunta'])
+    print()
+
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(opcoes):
+        print(f'Opção {i}) ' + opcao)
+    print()
+
+    escolha = input('Escolha uma opção: ')
+
+    acertou = False
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+
+    if escolha_int is not None:
+        resposta = pergunta['Resposta'] 
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == resposta:
+                acertou = True
+    
+    print()
+    if acertou:
+        print('Acertou')
+        qtd_acertos += 1
+    else:
+        print('Errou')
+
+    print()
+
+
+print(f'Você acertou: {qtd_acertos} de {qtd_perguntas} perguntas')
