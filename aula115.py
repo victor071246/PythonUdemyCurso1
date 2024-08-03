73790c8c5379fc96401007268e19cb91e40260a4
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-08-02 22:30:48 -0300

    Add files via upload

---
+++ aula115.py
@@ -0,0 +1,5 @@
+# Ambientes virtuais em Python (venv)
+# Um ambiente virtual carrega toda a sua instalação do Python para uma pasta no caminho escolhido.
+# Ao ativar um ambiente virtual, a instalação do ambiente virtual será utilizada.
+# Venv é o módulo que vamos utilizar para criar ambientes virtuais.
+# Você pode dar o nome que preferir para um ambiente virtual, mas os mais comuns são: venv | env | .venv | .env
