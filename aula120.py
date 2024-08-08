942b19f8d1dbb5c1a265086803a3c08aa65b677b
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-08-07 21:35:48 -0300

    Add files via upload

---
+++ aula120.py
@@ -0,0 +1,17 @@
+# class - Classes são moldes para criar novos objetos
+# As classes geram novos objetos (instâncias) que podem ter seus próprios métodos e atributos.
+# Os objetos gerados pelas classes podem usar seus dados internos para realizar várias ações
+# Por conveção, usamos PascalCase para nomes de classes
+# string = 'Luiz' # str
+# print(string.upper())
+# print(isinstance(string, str))  
+class Pessoa:
+    ...
+
+p1 = Pessoa()
+p1.nome = 'Luiz'
+p1.sobrenome = 'Otávio'
+
+
+print(p1.nome)
+print(p1.sobrenome)
