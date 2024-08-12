64f21d3d9a4af32337b2475b11ae1f748124c57d
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-08-11 23:24:47 -0300

    Add files via upload

---
+++ aula126.py
@@ -0,0 +1,26 @@
+# Atributos de classe
+
+class Pessoa:
+    ano_atual = 2022
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade
+
+dados = {'nome': 'João', 'idade': 35}
+p2 = Pessoa(**dados)
+p1 = Pessoa('João', 35)
+# p1.nome = 'EITA'
+# print(p1.idade)
+# p1.__dict__['outra'] = 'coisa'
+# p1.__dict__['nome'] = 'eita'
+# del p1.__dict__['nome']
+# print(p1.__dict__)
+# print(vars(p1))
+# print(p1.outra)
+# print(p1.nome)
+print(vars(p1))
+print(vars(p2))
