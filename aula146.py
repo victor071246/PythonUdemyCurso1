d85763d123b047055482526f9d32be6ce11ea686
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-08-21 21:11:37 -0300

    Add files via upload

--- aula146.py
+++ aula146.py
@@ -6,5 +6,24 @@
 # Levantando (raise) / Lançando trhow exceções
 # Relançando exceções
 # Adicionando notas em exceções (3.11.0)
-class MeuError(Exception):
+class MeuErro(Exception):
     ...
+
+class OutroErro(Exception):
+    ...
+
+def levantar():
+    exception_ =  MeuErro('A mensagem do meu erro')
+    exception_.add_note('Olha a nota 1')
+    exception_.add_note('Você errou isso')
+    raise exception_
+try:
+    levantar()
+except (MeuErro, ZeroDivisionError) as error:
+    print(error.__class__.__name__)
+    print(error.args)
+    print()
+    exception_ =  OutroErro('Vou lançar de novo')
+    exception_.__notes__ = error.__notes__.copy()
+    exception_.add_note('Mais uma nota')
+    raise exception_ from error
