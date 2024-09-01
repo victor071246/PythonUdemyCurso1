345d02b94c754a22da2d9b9b2bcdd7d33d241814
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-09-01 19:47:21 -0300

    Add files via upload

--- aula179.py
+++ aula179.py
@@ -1,10 +1,18 @@
 # csv.reader e csv.DictReader
 # csv.reader lê o CSV em formato de lista
 # csv.DictReader lê o csv em formato de dicionário
+import csv
 from pathlib import Path
+
+from aulas.aula136 import Endereco
 
 CAMINHO_CSV = Path(__file__).parent / 'aula179.csv'
 
 print(CAMINHO_CSV)
 
-with open(CAMINHO_CSV, 'r') as arquivo:z
+with open(CAMINHO_CSV, 'r') as arquivo:
+    leitor = csv.DictReader(arquivo)
+
+    # print(next(leitor))
+    for linha in leitor:
+        print(linha['Nome', linha['Idade'], linha['Endereco']])
