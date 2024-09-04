6f1ea376c44e0edf661b37e40f996070c0d64a96
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-09-03 23:34:47 -0300

    Add files via upload

---
+++ aula187.py
@@ -0,0 +1,15 @@
+# sys.argv - Executando arquivos com argumentos no sistema
+# Fonte = Fira Code
+import sys
+argumentos = sys.argv
+qtd_argumentos = len(argumentos)
+
+if qtd_argumentos <= 1:
+    print('Você não passou argumentos')
+else:
+    try:
+        print(f'Você passou os argumentos {argumentos[1:]}')
+        print('Faça alguma coisa com {argumentos[1]}')
+        print('Faça outra coisa com {argumentos[2]}')
+    except IndexError:
+        print('Faltam argumentos')
